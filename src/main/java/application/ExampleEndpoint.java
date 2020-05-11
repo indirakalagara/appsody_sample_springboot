599bfa64c475a43d0937111b8686ed06a2f0152f
IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
2020-05-11 14:34:58 +0530

    add logger stmt

--- src/main/java/application/ExampleEndpoint.java
+++ src/main/java/application/ExampleEndpoint.java
@@ -5,9 +5,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ExampleEndpoint {
+    private static final Logger LOGGER = LoggerFactory.getLogger(ExampleEndpoint.class);
 
     @RequestMapping("/starter/hello")
     public String example() {
+        LOGGER.trace("example sayHello ");
         return "Hello World again";
+
     }
 }
