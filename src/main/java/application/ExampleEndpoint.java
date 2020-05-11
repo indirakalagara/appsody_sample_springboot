4aaa836dfa14085f51cbacb1d27e721122674700
IndiraKalagara <indirakalagara@IndiraKalagaras-MacBook-Pro.local>
2020-05-11 15:13:58 +0530

    add logger imports

--- src/main/java/application/ExampleEndpoint.java
+++ src/main/java/application/ExampleEndpoint.java
@@ -2,6 +2,8 @@ package application;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @RestController
 public class ExampleEndpoint {
